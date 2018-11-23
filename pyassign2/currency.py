3c086fa2d3282889f63d5cacf941534c32abda2e
王雪峰 <43375392+Xuefeng-Wang@users.noreply.github.com>
2018-11-24 00:01:36 +0800

    Update currency.py

--- pyassign2/currency.py
+++ pyassign2/currency.py
@@ -44,10 +44,8 @@ def currency_response(currency_from, currency_to, amount_from):
     Precondition: currency_to is a string
     
     Parameter amount_from: amount of currency to convert
-    Precondition: amount_from is a float"""
-        
-    amount_from = str(amount_from)
-    
+    Precondition: amount_from is a string"""
+
     from urllib.request import urlopen
 
     doc = urlopen('http://cs1110.cs.cornell.edu/2016fa/a1server.php?from='+currency_from+'&to='+currency_to+'&amt='+amount_from)
@@ -62,45 +60,53 @@ def iscurrency(currency):
     Parameter currency: the currency code to verify
     Precondition: currency is a string."""
     
-    jstr = currency_response(currency, currency, 1.0)
+    jstr = currency_response(currency, currency, "1.0")
     return not has_error(jstr)
 
 def exchange(currency_from, currency_to, amount_from):
-    """Returns: amount of currency received in the given exchange.
+    """Returns: amount of currency received in the given exchange or reporting the error.
 
     Parameter currency_from: the currency on hand
-    Precondition: currency_from is a string for a valid currency code
-    
+   
     Parameter currency_to: the currency to convert to
-    Precondition: currency_to is a string for a valid currency code
     
-    Parameter amount_from: amount of currency to convert
-    Precondition: amount_from is a float"""
+    Parameter amount_from: amount of currency to convert"""
     
     jstr = currency_response(currency_from, currency_to, amount_from)
-    if iscurrency(currency_from) and iscurrency(currency_to):
-        x = get_to(jstr)
-        amount_to_str = before_space(x)
-        return float(amount_to_str)
-    elif not iscurrency(currency_from) and iscurrency(currency_to):
-        return "Source currency code is invalid."
-    elif iscurrency(currency_from) and not iscurrency(currency_to):
-        return "Exchange currency code is invalid."
-    else:
-        return "Source currency code and exchange currency code are invalid."
+    try:
+        float(amount_from)
+        if iscurrency(currency_from) and iscurrency(currency_to):
+            x = get_to(jstr)
+            amount_to_str = before_space(x)
+            return float(amount_to_str)
+        elif not iscurrency(currency_from) and iscurrency(currency_to):
+            return "Source currency code is invalid."
+        elif iscurrency(currency_from) and not iscurrency(currency_to):
+            return "Exchange currency code is invalid."
+        else:
+            return "Source currency code and exchange currency code are invalid."
+    except ValueError:
+        if iscurrency(currency_from) and iscurrency(currency_to):
+            return "Currency amount is invalid."
+        elif not iscurrency(currency_from) and iscurrency(currency_to):
+            return "Source currency code and currency amount are invalid."
+        elif iscurrency(currency_from) and not iscurrency(currency_to):
+            return "Exchange currency code and currency amount are invalid."
+        else:
+            return "Source currency code, exchange currency code and currency amount are invalid."
         
 def test_currency_response():
     """test function currency_response(currency_from, currency_to, amount_from)"""
-    assert(currency_response("USD", "EUR", 2.5)==
+    assert(currency_response("USD", "EUR", "2.5")==
            '{ "from" : "2.5 United States Dollars", "to" : "2.1589225 Euros", "success" : true, "error" : "" }')
-    assert(currency_response("CNY", "JPY", 1)==
+    assert(currency_response("CNY", "JPY", "1")==
            '{ "from" : "1 Chinese Yuan", "to" : "16.257643642095 Japanese Yen", "success" : true, "error" : "" }')
-    assert(currency_response("AAA","USD",3.1)==
+    assert(currency_response("AAA","USD","3.1")==
            '{ "from" : "", "to" : "", "success" : false, "error" : "Source currency code is invalid." }')
-    assert(currency_response("EUR","XYZ",2.8)==
+    assert(currency_response("EUR","XYZ","2.8")==
            '{ "from" : "", "to" : "", "success" : false, "error" : "Exchange currency code is invalid." }')
-    assert(currency_response("AAA","XYZ",2.8)==
-           '{ "from" : "", "to" : "", "success" : false, "error" : "Source currency code is invalid." }')
+    assert(currency_response("EUR","CNY","a")==
+           '{ "from" : "", "to" : "", "success" : false, "error" : "Currency amount is invalid." }')
 
 def test_has_error():
     """test function has_error(jstr)"""
@@ -127,11 +133,15 @@ def test_before_space():
 
 def test_exchange():
     """test function exchange(currency_from, currency_to, amount_from)"""
-    assert(2.1589225 == exchange("USD", "EUR", 2.5))
-    assert(16.257643642095 == exchange("CNY", "JPY", 1))
-    assert("Source currency code is invalid." == exchange("AAA", "USD", 1.2))
-    assert("Exchange currency code is invalid." == exchange("EUR","XYZ",3.4))
-    assert("Source currency code and exchange currency code are invalid." == exchange("eur","usd",2.88))
+    assert(2.1589225 == exchange("USD", "EUR", "2.5"))
+    assert(16.257643642095 == exchange("CNY", "JPY", "1"))
+    assert("Source currency code is invalid." == exchange("AAA", "USD", "1.2"))
+    assert("Exchange currency code is invalid." == exchange("EUR","XYZ","3.4"))
+    assert("Source currency code and exchange currency code are invalid." == exchange("eur","usd","2.88"))
+    assert("Currency amount is invalid." == exchange("USD","EUR","a"))
+    assert("Source currency code and currency amount are invalid." == exchange("aaa","EUR","a"))
+    assert("Exchange currency code and currency amount are invalid." == exchange("USD","aaa","a"))
+    assert("Source currency code, exchange currency code and currency amount are invalid." == exchange("aaa","bbb","a"))
 
 def test_all():
     """test all cases"""
@@ -148,7 +158,7 @@ def main():
     test_all()
     currency_from = input("Please enter the currency on hand:")
     currency_to = input("Please enter the currency to convert to:")
-    amount_from = float(input("Please enter the amount of currency to convert:"))
+    amount_from = input("Please enter the amount of currency to convert:")
     amount_to = exchange(currency_from, currency_to, amount_from)
     if type(amount_to) == float:
         print("The amount of currency received in the given exchange is "+str(amount_to)+".")
